b1ac7426a3787256e4f14ca5818b246ab784f672
Usman <usman.khaliq91@gmail.com>
2017-08-25 22:52:18 +0000

    test commit

--- src/main/java/xyz/idtlabs/smsgateway/tenants/api/TenantsApiResource.java
+++ src/main/java/xyz/idtlabs/smsgateway/tenants/api/TenantsApiResource.java
@@ -15,7 +15,8 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */
+ */ 
+//testing this out
 package xyz.idtlabs.smsgateway.tenants.api;
 
 import xyz.idtlabs.smsgateway.tenants.domain.Tenant;
