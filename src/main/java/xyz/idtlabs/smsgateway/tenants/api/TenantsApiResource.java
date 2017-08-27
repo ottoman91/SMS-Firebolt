465cb0a1c9fd60144e230e6e8b45868950b2547e
Usman <usman.khaliq91@gmail.com>
2017-08-27 00:45:22 +0000

    changes

--- src/main/java/xyz/idtlabs/smsgateway/tenants/api/TenantsApiResource.java
+++ src/main/java/xyz/idtlabs/smsgateway/tenants/api/TenantsApiResource.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */ 
-//testing this out
+
 package xyz.idtlabs.smsgateway.tenants.api;
 
 import xyz.idtlabs.smsgateway.tenants.domain.Tenant;
