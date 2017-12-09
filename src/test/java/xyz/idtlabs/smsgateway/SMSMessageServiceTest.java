dc36fbe269d0aa304f131f726ec4e55c19bd5dc1
Usman <usman.khaliq91@gmail.com>
2017-12-09 00:39:11 +0000

    edited test suite

--- src/test/java/xyz/idtlabs/smsgateway/SMSMessageServiceTest.java
+++ src/test/java/xyz/idtlabs/smsgateway/SMSMessageServiceTest.java
@@ -19,6 +19,7 @@
 
 package xyz.idtlabs.smsgateway;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -26,13 +27,28 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import xyz.idtlabs.smsgateway.configuration.SmsFireboltConfiguration;
 import xyz.idtlabs.smsgateway.exception.PlatformApiDataValidationException;
 import xyz.idtlabs.smsgateway.exception.PlatformApiInvalidParameterException;
+import xyz.idtlabs.smsgateway.sms.domain.SMSMessage;
+import xyz.idtlabs.smsgateway.sms.repository.SmsOutboundMessageRepository;
 import xyz.idtlabs.smsgateway.sms.service.SMSMessageService;
+import xyz.idtlabs.smsgateway.tenants.domain.Tenant;
+import xyz.idtlabs.smsgateway.tenants.repository.TenantRepository;
+import xyz.idtlabs.smsgateway.tenants.service.TenantsService;
+
+import javax.validation.constraints.AssertTrue;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 import static org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection.H2;
 
 @RunWith(SpringRunner.class)
@@ -43,12 +59,26 @@ import static org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnec
 public class SMSMessageServiceTest {
 
 
-
     @Autowired
     private SMSMessageService smsMessageService;
+    @Autowired
+    private TenantsService tenantService;
+
+    @Autowired
+    private TenantRepository tenantRepository;
+
+    @Autowired
+    private SmsOutboundMessageRepository smsOutboundMessageRepository;
+
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    @After
+    public void tearDown() throws Exception {
+        tenantRepository.deleteAll();
+        smsOutboundMessageRepository.deleteAll();
+    }
 
     @Test
     public void validateMessageAndDestination_FailWhenErrorNotThrownForEmptyMessage(){
@@ -145,4 +175,84 @@ public class SMSMessageServiceTest {
         String numbers = "+23277775775,+23277776776";
         smsMessageService.validateMessageAndDestination(numbers,message);
     }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    public void findMessagesByTenantId_FailWhenMessagesFromOneClientNotFoundFromDatabase(){
+
+        Tenant testClient = new Tenant("defaultApiKey","testClient","testClientDisplay");
+        Tenant savedTestClient = tenantService.createTenant(testClient);
+        char[] chars = new char[160];
+        Arrays.fill(chars,'a');
+        String message1 = new String(chars);
+
+        char[] chars2 = new char[160];
+        Arrays.fill(chars2,'b');
+        String message2 = new String(chars2);
+        String number = "+23277775775";
+        smsMessageService.saveSMS(savedTestClient.getApiKey(),number,message1);
+        smsMessageService.saveSMS(savedTestClient.getApiKey(),number,message2);
+        Page<SMSMessage> smsMessage = smsMessageService.findMessagesByTenantId(savedTestClient.getId(),0,2);
+        assertEquals("Messages sent by client not retrieved",2,smsMessage.getTotalElements());
+    }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    public void findMessagesByTenantIdAndId_FailWhenSingleMessageFromClientNotFoundFromDatabase(){
+
+        Tenant testClient = new Tenant("defaultApiKey","testClient","testClientDisplay");
+        Tenant savedTestClient = tenantService.createTenant(testClient);
+        char[] chars = new char[160];
+        Arrays.fill(chars,'a');
+        String message1 = new String(chars);
+        String number = "+23277775775";
+        smsMessageService.saveSMS(savedTestClient.getApiKey(),number,message1);
+        long messageId = smsOutboundMessageRepository.findByTenantId(savedTestClient.getId()).getId();
+        SMSMessage smsMessage = smsMessageService.findMessageByTenantIdAndId(savedTestClient.getId(),messageId);
+        assertEquals("Unable to retrieve single message sent by client ",message1,smsMessage.getMessage());
+    }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    public void showTotalMessagesSentBetweenDatesByTenant_FailWhenMessagesSentBetweenDatesNotRetrievedFromDatabase(){
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date currentDate = new Date();
+        Date startingDate = new Date();
+        String date = "2011-01-01";
+        try {
+
+            startingDate = format.parse(date);
+        } catch (java.text.ParseException e) {
+        }
+
+
+        String currentStringDate = currentDate.toString();
+        try{
+            currentDate = format.parse(currentStringDate);
+        }catch(java.text.ParseException e){
+
+        }
+
+        Tenant testClient = new Tenant("defaultApiKey","testClient","testClientDisplay");
+        Tenant savedTestClient = tenantService.createTenant(testClient);
+        char[] chars = new char[160];
+        Arrays.fill(chars,'a');
+        String message = new String(chars);
+
+        char[] chars2 = new char[160];
+        Arrays.fill(chars2,'b');
+        String message1 = new String(chars2);
+
+        String number = "+23277775775";
+        smsMessageService.saveSMS(savedTestClient.getApiKey(),number,message);
+        smsMessageService.saveSMS(savedTestClient.getApiKey(),number,message1);
+
+        int numberOfMessagesSent = smsMessageService.showTotalMessagesSentBetweenDatesByTenant(savedTestClient.getId(),
+                startingDate,currentDate);
+
+        assertEquals("Messages between specific dates were not recovered",2,numberOfMessagesSent);
+    }
+
+
 }
