ca769611f0b8d34f9b50c3902bc24b4d2cef060f
Usman <usman.khaliq91@gmail.com>
2018-01-21 16:39:47 +0000

    fix commands to script

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -4,3 +4,5 @@ sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y --allow-unauthenticated -o Dpkg::Options::=--force-confnew mysql-server
 sudo mysql_upgrade -u root sudo service mysql restart
+mysql -uroot -pmysql -e 'create database messagegateway;'
+mysql -uroot -pmysql messagegateway < "src/main/resources/db/migration/V1__Initial_Setup.sql"
