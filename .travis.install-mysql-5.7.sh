284a5109e848482056b325998a2355572d22fce6
Usman <usman.khaliq91@gmail.com>
2018-01-21 19:10:23 +0000

    added mysql lines to before_script

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -4,6 +4,3 @@ sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y --allow-unauthenticated -o Dpkg::Options::=--force-confnew mysql-server
 sudo mysql_upgrade -u root sudo service mysql restart
-sudo mysql -uroot -pmysql -e 'create database messagegateway;'
-sudo mysql -uroot -pmysql -e 'USE messagegateway;'
-sudo mysql -uroot -pmysql messagegateway < src/main/resources/db/migration/V1__Initial_Setup.sql
